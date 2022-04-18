4b70f611c6e6268e77f7fcee5645027fe5ac2e5f
michael-malling <mmalling@salesforce.com>
2022-04-18 14:58:52 -0400

    Added support for folder-based metadata (staticresources & documents)

--- test/lib/setup.ts
+++ test/lib/setup.ts
@@ -21,10 +21,11 @@ export default class Setup {
 
         var deltaKind = 'A'
         var myPath = folder;
+        var filePath = null;
         await Utils.mkDirPath(myPath);
         for (let x = 0; x < count; x++) {
             for (let y = 0; y < count; y++) {
-                var filePath = path.join(myPath, `myfile.${y}.txt`);
+                filePath = path.join(myPath, `myfile.${y}.txt`);
                 await fs.appendFile(filePath, y + '\r\n');
                 await fs.appendFile(Setup.md5FilePath, `${filePath}=${y}\r\n`);
                 await fs.appendFile(Setup.gitFilePath, `${deltaKind}\t${filePath}\r\n`);
@@ -38,5 +39,20 @@ export default class Setup {
             myPath = path.join(myPath, 'sub_' + x);
             await Utils.mkDirPath(myPath);
         }
+
+        //create staticresources folder structure
+        filePath = path.join(folder,'folder.resource-meta.xml'); 
+        await fs.appendFile(filePath,'1\r\n');
+        yield filePath;
+
+        var folderPath = path.join(folder,'folder');
+        await Utils.mkDirPath(folderPath);
+        
+        filePath = path.join(folderPath,'file1.txt'); 
+        await fs.appendFile(filePath,'1\r\n');
+        yield filePath;
+
+        await fs.appendFile(Setup.md5FilePath, `${filePath}=1\r\n`);
+        await fs.appendFile(Setup.gitFilePath, `${deltaKind}\t${filePath}\r\n`);
     }
 }
