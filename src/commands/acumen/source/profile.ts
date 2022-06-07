0d337268a519548102a7c7f2dbbf359df90fdfd7
michael-malling <mmalling@salesforce.com>
2022-06-07 12:21:50 -0400

    Deprecated command

--- src/commands/acumen/source/profile.ts
+++ src/commands/acumen/source/profile.ts
@@ -1,9 +1,11 @@
 import { flags } from '@salesforce/command';
 import { CommandBase } from '../../../lib/command-base';
+/*
 import Utils from '../../../lib/utils';
 import path = require('path');
 import { SfdxPermission, PermissionSet } from '../../../lib/sfdx-permission';
 import { SfdxTasks } from '../../../lib/sfdx-tasks';
+*/
 
 export default class Profile extends CommandBase {
   public static defaultSourceFolder: string = null;
@@ -37,106 +39,107 @@ export default class Profile extends CommandBase {
     })
   };
 
-  protected static requiresProject = false;
-  protected static requiresUsername = true;
+  //protected static requiresProject = false;
+  //protected static requiresUsername = true;
 
-  protected permissions: Map<string, PermissionSet>;
+  //protected permissions: Map<string, PermissionSet>;
 
   public async run(): Promise<void> {
-    const sourceFolders = !this.flags.source
-      ? Profile.defaultPermissionsGlobs
-      : this.flags.source.split(',');
+    this.ux.log(`\tThis command has been depricated.`);
+    // const sourceFolders = !this.flags.source
+    //   ? Profile.defaultPermissionsGlobs
+    //   : this.flags.source.split(',');
 
-    this.ux.log('Gathering securable metadata information from Org:');
-    this.ux.log(`${SfdxPermission.defaultPermissionMetaTypes}`);
-    const metaDataMap = await SfdxTasks.listMetadatas(this.orgAlias, SfdxPermission.defaultPermissionMetaTypes);
+    // this.ux.log('Gathering securable metadata information from Org:');
+    // this.ux.log(`${SfdxPermission.defaultPermissionMetaTypes}`);
+    // const metaDataMap = await SfdxTasks.listMetadatas(this.orgAlias, SfdxPermission.defaultPermissionMetaTypes);
 
-    this.permissions = new Map<string, PermissionSet>();
+    // this.permissions = new Map<string, PermissionSet>();
 
-    let gotStandardTabs = false;
+    // let gotStandardTabs = false;
 
-    for (const sourceFolder of sourceFolders) {
-      if (!sourceFolder) {
-        continue;
-      }
-      this.ux.log(`Reading metadata in: ${sourceFolder}`);
-      for await (const filePath of Utils.getFiles(sourceFolder.trim())) {
-        this.ux.log(`\tProcessing: ${filePath}`);
-        const json = await Utils.readObjectFromXmlFile(filePath);
+    // for (const sourceFolder of sourceFolders) {
+    //   if (!sourceFolder) {
+    //     continue;
+    //   }
+    //   this.ux.log(`Reading metadata in: ${sourceFolder}`);
+    //   for await (const filePath of Utils.getFiles(sourceFolder.trim())) {
+    //     this.ux.log(`\tProcessing: ${filePath}`);
+    //     const json = await Utils.readObjectFromXmlFile(filePath);
 
-        if (!json.PermissionSet && !json.Profile) {
-          this.ux.log(`\tUnable to process file: ${filePath}`);
-          continue;
-        }
+    //     if (!json.PermissionSet && !json.Profile) {
+    //       this.ux.log(`\tUnable to process file: ${filePath}`);
+    //       continue;
+    //     }
 
-        const permSet = PermissionSet.fromXml(filePath, json);
-        const newPermSet = new PermissionSet();
-        for (const [metadataName, members] of metaDataMap) {
-          const permSetErrors = [];
-          const existingNames = [];
-          const standardTabs = [];
+    //     const permSet = PermissionSet.fromXml(filePath, json);
+    //     const newPermSet = new PermissionSet();
+    //     for (const [metadataName, members] of metaDataMap) {
+    //       const permSetErrors = [];
+    //       const existingNames = [];
+    //       const standardTabs = [];
 
-          const permCollection = permSet.getPermissionCollection(metadataName);
-          const newPermCollection = newPermSet.getPermissionCollection(metadataName);
+    //       const permCollection = permSet.getPermissionCollection(metadataName);
+    //       const newPermCollection = newPermSet.getPermissionCollection(metadataName);
 
-          switch (metadataName) {
-            case SfdxPermission.customTab:
-              if (permSet.tabVisibilities) {
-                for (const [tabName, tabPerm] of permCollection) {
-                  if (tabPerm['isStandard']) {
-                    // Standard Tabs are not exposed via the Metadata API
-                    standardTabs.push(tabName);
-                    gotStandardTabs = true;
-                    continue;
-                  }
-                  existingNames.push(tabName);
-                }
-              }
-              break;
-            default:
-              if (permCollection) {
-                existingNames.push(...permCollection.keys());
-              }
-          }
-          const typeNames = new Set(members);
-          for (const existingName of existingNames) {
-            if (!typeNames.has(existingName)) {
-              permSetErrors.push(`${existingName} NOT found.`);
-            } else {
-              newPermCollection.set(existingName, permCollection.get(existingName));
-            }
-          }
+    //       switch (metadataName) {
+    //         case SfdxPermission.customTab:
+    //           if (permSet.tabVisibilities) {
+    //             for (const [tabName, tabPerm] of permCollection) {
+    //               if (tabPerm['isStandard']) {
+    //                 // Standard Tabs are not exposed via the Metadata API
+    //                 standardTabs.push(tabName);
+    //                 gotStandardTabs = true;
+    //                 continue;
+    //               }
+    //               existingNames.push(tabName);
+    //             }
+    //           }
+    //           break;
+    //         default:
+    //           if (permCollection) {
+    //             existingNames.push(...permCollection.keys());
+    //           }
+    //       }
+    //       const typeNames = new Set(members);
+    //       for (const existingName of existingNames) {
+    //         if (!typeNames.has(existingName)) {
+    //           permSetErrors.push(`${existingName} NOT found.`);
+    //         } else {
+    //           newPermCollection.set(existingName, permCollection.get(existingName));
+    //         }
+    //       }
 
-          if (standardTabs.length > 0) {
-            for (const standardTab of standardTabs) {
-              permSetErrors.push(`\t${standardTab} (*)`);
-            }
-          }
-          if (permSetErrors.length > 0) {
-            this.ux.log(`\t\t${metadataName}:`);
-            for (const error of permSetErrors) {
-              this.ux.log(`\t\t\t${error}`);
-            }
-          }
+    //       if (standardTabs.length > 0) {
+    //         for (const standardTab of standardTabs) {
+    //           permSetErrors.push(`\t${standardTab} (*)`);
+    //         }
+    //       }
+    //       if (permSetErrors.length > 0) {
+    //         this.ux.log(`\t\t${metadataName}:`);
+    //         for (const error of permSetErrors) {
+    //           this.ux.log(`\t\t\t${error}`);
+    //         }
+    //       }
 
-        }
-        if (this.flags.modify) {
-          const outFilePath = this.flags.output
-            ? path.join(this.flags.output, filePath)
-            : filePath;
+    //     }
+    //     if (this.flags.modify) {
+    //       const outFilePath = this.flags.output
+    //         ? path.join(this.flags.output, filePath)
+    //         : filePath;
 
-          this.ux.log(`\tUpdating: ${outFilePath}`);
-          await Utils.writeObjectToXmlFile(outFilePath, newPermSet.toXmlObj());
-        }
-      }
-    }
-    if (gotStandardTabs) {
-      this.ux.log('(*) WARNING: Standard Tab permissions detected.');
-      this.ux.log('Salesforce does not expose Standard Tabs via the Metadata API.');
-      this.ux.log(`Compatibility with '${this.orgAlias}' can only be ensured if these permissions are removed.`);
-    }
+    //       this.ux.log(`\tUpdating: ${outFilePath}`);
+    //       await Utils.writeObjectToXmlFile(outFilePath, newPermSet.toXmlObj());
+    //     }
+    //   }
+    // }
+    // if (gotStandardTabs) {
+    //   this.ux.log('(*) WARNING: Standard Tab permissions detected.');
+    //   this.ux.log('Salesforce does not expose Standard Tabs via the Metadata API.');
+    //   this.ux.log(`Compatibility with '${this.orgAlias}' can only be ensured if these permissions are removed.`);
+    // }
 
-    this.ux.log('Done.');
+    // this.ux.log('Done.');
 
     return;
   }
